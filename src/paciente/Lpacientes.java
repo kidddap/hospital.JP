588a71260e9395a834688db02374d5208733fb95
kidddap <vpastranaaxel@gmail.com>
2023-11-28 09:26:46 -0600

    Se procede Subir las clases actualizadas y un formulario
    Las respectivas clases son hospital, Pacientes y la que controla la lista de pacientes Lpacientes ademas del formulario

--- src/paciente/Lpacientes.java
+++ src/paciente/Lpacientes.java
@@ -4,10 +4,49 @@
  */
 package paciente;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  *
  * @author Student
  */
-public class Lpacientes {
+public class Lpacientes extends Pacientes{
+    protected ArrayList <Pacientes> ListaPa= new ArrayList<>();
+
+    public Lpacientes(int numCedu, String name, String date, Date horallega, String clasificacion, Date horasali) {
+        super(numCedu, name, date, horallega, clasificacion, horasali);
+    }
+
+
+  
+    private void addPaciente(Pacientes paci){
+        if ("Rojo".equals(this.clasificacion)) {
+            ListaPa.add(0, paci);
+        }
+        if ("Amarillo".equals(this.clasificacion)) {
+            ListaPa.add(1, paci);
+        }
+        if ("verde".equals(this.clasificacion)) {
+            ListaPa.add(paci);
+        }
+    }
+    
+    private boolean atenderPaci(Pacientes paci, boolean atendido){
+        for (Pacientes ListaPa1 : ListaPa) {
+            if (atendido==true) {
+                return " ya se puede ir ".equals(atendido);
+            } else {
+            }
+        }
+        return false;
+       
+}
+    private void searchPaci(){
+        for(Pacientes p:ListaPa){
+            System.out.println(p);
+        }
+        
+    }
     
 }
--- src/paciente/Pacientes.java
+++ src/paciente/Pacientes.java
@@ -4,10 +4,81 @@
  */
 package paciente;
 
+import java.util.Date;
+
 /**
  *
  * @author Student
  */
 public class Pacientes {
-    
+    protected int numCedu;
+    protected String name;
+    protected String date;
+    protected Date horallega;
+    protected String clasificacion;
+    protected Date horasali;
+
+    public int getNumCedu() {
+        return numCedu;
+    }
+
+    public void setNumCedu(int numCedu) {
+        this.numCedu = numCedu;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Date getHorallega() {
+        return horallega;
+    }
+
+    public void setHorallega(Date horallega) {
+        this.horallega = horallega;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public Date getHorasali() {
+        return horasali;
+    }
+
+    public void setHorasali(Date horasali) {
+        this.horasali = horasali;
+    }
+
+    public Pacientes(int numCedu, String name, String date, Date horallega, String clasificacion, Date horasali) {
+        this.numCedu = numCedu;
+        this.name = name;
+        this.date = date;
+        this.horallega = horallega;
+        this.clasificacion = clasificacion;
+        this.horasali = horasali;
+    }
+
+    @Override
+    public String toString() {
+        return "Pacientes{" + "numCedu=" + numCedu + ", name=" + name + ", date=" + date + ", horallega=" + horallega + ", clasificacion=" + clasificacion + ", horasali=" + horasali + '}';
+    }
+
+   
 }
